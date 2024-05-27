c7f80d3a4c3eff0519a1ccb287ca69b3418ce2c2
Shivang Tiwari <shivangtiwari7011@gmail.com>
2024-05-27 18:35:29 +0530

    changed

--- src/component/Notes/AddNote.js
+++ src/component/Notes/AddNote.js
@@ -1,9 +1,11 @@
+//parent 
+
 import React, { useContext, useState } from 'react';
 import noteContext from '../.././Context/Note/NoteContext';
 
 const AddNote = () => {
+
     const context = useContext(noteContext);
-    const [loading, setLoading] = useState(false);
     const { addNote } = context;
 
     const [note, setNote] = useState({ title: "", description: "", tag: "default" });
@@ -13,62 +15,36 @@ const AddNote = () => {
     }
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        try {
-            setLoading(true);
-            addNote(note.title, note.description, note.tag);
-        } catch (error) {
-            console.log("Something went wrong", error);
-        } finally {
-            setLoading(false);
-        }
+        e.preventDefault()
+        addNote(note.title, note.description, note.tag);
     }
 
     return (
-        <div className='container my-3 '>
+        <div className='container my-3 ' onClick={handleSubmit}>
             <h2>Your Notes</h2>
             <form className='my-3'>
 
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Title</label>
-                    <input
-                        type="text"
+                    <input type="text"
                         className="form-control"
                         id="title"
                         name="title"
                         aria-describedby="emailHelp"
-                        required={true}
-                        value={note.title}
-                        onChange={onChange}
-                    />
+                        onChange={onChange} />
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="description" className="form-label">Note</label>
-                    <textarea
-                        className="form-control"
-                        id="description"
-                        name='description'
-                        required={true}
-                        value={note.description}
-                        onChange={onChange}
-                        rows={5}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="tag" className="form-label">Tag</label>
+                    <label htmlFor="description" className="form-label">Description</label>
                     <input
                         type="text"
                         className="form-control"
-                        id="tag"
-                        name='tag'
-                        required={true}
-                        value={note.tag}
+                        id="description"
+                        name='description'
                         onChange={onChange} />
                 </div>
-                <button disabled={note.title.length < 5 || note.description.length < 5} type="button" className="btn btn-primary" onClick={handleSubmit}>
-                    {loading ? `Adding..` : `Add`}
-                </button>
+                <button disabled={note.title.length < 5 || note.description.length < 5} type="button" className="btn btn-primary">Add note</button>
+
             </form>
         </div>
     );
